876ec04d470ffb9bc2717b81685c6b5b5c35b51c
Samuel Luciano <sa.lassis@gmail.com>
2019-07-07 23:02:00 -0400

    Comment out CompatibilityVersion from Startup class...

--- ControlMe.WebApi/Startup.cs
+++ ControlMe.WebApi/Startup.cs
@@ -25,7 +25,7 @@ namespace ControlMe.WebApi {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()/*.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)*/;
 
             services.AddDbContext<ControlMeDatabaseContext>(options => {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"));

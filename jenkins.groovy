4d2502aaf17a0ddd32fd8019e4ac239f637e006e
Samuel Luciano Lassis <samuelito85@hotmail.com>
2019-07-14 01:21:05 -0400

    Add steps to jenkins.groovy file....

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,8 +2,10 @@ pipeline{
   agent any
   stages{
     stage("build"){
-      script{
-        sh 'dotnet --version'
+      steps{
+        script{
+          sh 'dotnet --version'
+        }
       }
     }
   }

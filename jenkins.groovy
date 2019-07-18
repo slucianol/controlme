8e1ff99b52dd2ec74105b51fa9b2681b62f918a0
Samuel Luciano Lassis <samuelito85@hotmail.com>
2019-07-18 11:39:04 -0400

    Add stages to jenkins pipeline and sonarqube scanning...

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,6 +1,13 @@
 pipeline{
 	agent any
 	stages{
+		stage("code_analysis"){
+			steps{
+				script{
+					sh 'sonar-scanner'
+				}
+			}
+		}
 		stage("build"){
 			steps{
 				script{
@@ -9,10 +16,34 @@ pipeline{
 						sh 'dotnet restore'
 						sh 'dotnet build --configuration Release --output ../artifacts --no-restore'
 					}
+				}
+			}
+		}
+		stage("unit_test"){
+			steps{
+				script{
 					dir("${env.WORKSPACE}/ControlMe.Tests"){
 						sh 'dotnet test --configuration Release'
 					}
 				}
+			}
+		}
+		stage("deployment"){
+			steps{
+				script{
+					echo 'Deployment to azure web app'
+				}
+			}
+		}
+		stage("tests"){
+			steps{
+				parallel load:{
+					echo 'Executing load testing...'
+				},
+				functional:{
+					echo 'Executing functional testing...'
+				},
+				failFast: true
 			}
 		}
 	}

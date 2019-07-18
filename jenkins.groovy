8dc98d709b6978db99ef7f2a0f3b8f19e667207a
Samuel Luciano Lassis <samuelito85@hotmail.com>
2019-07-18 19:01:04 -0400

    Refine the deployment step for adding a slot creation for the deploy...

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,9 @@
 pipeline{
 	agent any
+	enviroment{
+		WORKING_BRANCH = env.BRANCH_NAME.split('/')[1]
+		SLOT_CREATED = false
+	}
 	stages{
 		stage("code_analysis"){
 			steps{
@@ -14,7 +18,7 @@ pipeline{
 					dir("${env.WORKSPACE}/ControlMe.WebApi"){
 						sh 'dotnet clean'
 						sh 'dotnet restore'
-						sh 'dotnet build --configuration Release --output ../artifacts --no-restore'
+						sh 'dotnet publish --configuration Release --output ../artifacts --no-restore'
 					}
 				}
 			}
@@ -31,8 +35,11 @@ pipeline{
 		stage("deployment"){
 			steps{
 				script{
-					dir("${env.WORKSPACE}"){
-						azureWebAppPublish appName: 'api-controlme', azureCredentialsId: 'Jenkins Integration Azure Service Principal',  filePath: '*.*', publishType: 'file', resourceGroup: 'RG_DEVOPS_DAY', slotName: 'development', sourceDirectory: 'artifacts', targetDirectory: ''
+					dir("${env.WORKSPACE}/artifacts"){
+						sh 'zip ControlMe.zip *'
+						sh "az webapp deployment slot create --resource-group RG_DEVOPS_DAY --name api-controlme --slot ${WORKING_BRANCH}"
+						SLOT_CREATED = true
+						sh "az webapp deployment source config-zip --resource-group RG_DEVOPS_DAY --name api-controlme --src ControlMe.zip --slot ${WORKING_BRANCH}"
 					}
 				}
 			}
@@ -46,6 +53,14 @@ pipeline{
 					echo 'Executing functional testing...'
 				},
 				failFast: true
+			}
+		}
+	}
+	post{
+		always{
+			if(SLOT_CREATED){
+				echo 'Remove created slot...'
+				//sh "az webapp deployment slot delete --resource-group RG_DEVOPS_DAY --name api-controlme --slot ${WORKING_BRANCH}"
 			}
 		}
 	}

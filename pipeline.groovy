3926b63fca8a2730e76113a1a3d1a545b84eccd9
Samuel Luciano Lassis <samuelito85@hotmail.com>
2019-08-28 13:13:04 -0400

    Testing Jenkins Demo

---
+++ pipeline.groovy
@@ -0,0 +1,14 @@
+pipeline{
+    agent any
+    stages{
+        stage("build"){
+            steps{
+                dir("ControlMe.WebApi"){
+                    sh "dotnet clean"
+                    sh "dotnet restore"
+                    sh "dotnet publish --no-restore --configuration Release --output ../artifacts"
+                }
+            }
+        }
+    }
+}
